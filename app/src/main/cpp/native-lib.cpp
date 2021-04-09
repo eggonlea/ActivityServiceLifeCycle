a08e8897e888b42022efdb4fb78d93a2d0ac78a7
Li Li <eggonlea@msn.com>
2021-04-09 13:13:57 -0700

    add native thread
    Signed-off-by: Li Li <eggonlea@msn.com>

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,28 @@
 #include <jni.h>
 #include <string>
+#include <thread>
+#include <unistd.h>
+#include <android/log.h>
+
+#define TAG "LifeCycleStdThread"
+
+bool finished = false;
+
+void run()
+{
+    int count = 0;
+    while (!finished) {
+        sleep(1);
+        __android_log_print(ANDROID_LOG_INFO, TAG, "%d", count++);
+    }
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_activityservicelifecycle_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    std::thread t(run);
+    t.detach();
     return env->NewStringUTF(hello.c_str());
 }
