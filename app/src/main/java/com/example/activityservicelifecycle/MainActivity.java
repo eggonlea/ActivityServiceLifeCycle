3ecf7e78ebadd3203f0d2a2784b6b496466d524b
Li Li <eggonlea@msn.com>
2021-04-09 12:52:30 -0700

    add AsyncTask
    Signed-off-by: Li Li <eggonlea@msn.com>

--- app/src/main/java/com/example/activityservicelifecycle/MainActivity.java
+++ app/src/main/java/com/example/activityservicelifecycle/MainActivity.java
@@ -1,78 +1,34 @@
 package com.example.activityservicelifecycle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.widget.TextView;
 import com.example.activityservicelifecycle.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-  private AppBarConfiguration appBarConfiguration;
+  // Used to load the 'native-lib' library on application startup.
+  static {
+    System.loadLibrary("native-lib");
+  }
+
   private ActivityMainBinding binding;
   private final static String TAG="LifeCycleActivity";
+  private MyAsyncTask asyncTask = new MyAsyncTask();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     Log.i(TAG, "onCreate");
+    asyncTask.execute();
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
 
-    setSupportActionBar(binding.toolbar);
-
-    NavController navController = Navigation
-        .findNavController(this, R.id.nav_host_fragment_content_main);
-    appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-    binding.fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    Log.i(TAG, "onCreateOptionsMenu");
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-  @Override
-  public boolean onSupportNavigateUp() {
-    NavController navController = Navigation
-        .findNavController(this, R.id.nav_host_fragment_content_main);
-    return NavigationUI.navigateUp(navController, appBarConfiguration)
-        || super.onSupportNavigateUp();
+    // Example of a call to a native method
+    TextView tv = binding.sampleText;
+    tv.setText(stringFromJNI());
   }
 
   @Override
@@ -104,4 +60,10 @@ public class MainActivity extends AppCompatActivity {
     super.onDestroy();
     Log.i(TAG, "onStop");
   }
+
+  /**
+   * A native method that is implemented by the 'native-lib' native library, which is packaged with
+   * this application.
+   */
+  public native String stringFromJNI();
 }
---
+++ app/src/main/java/com/example/activityservicelifecycle/MyAsyncTask.java
@@ -0,0 +1,21 @@
+package com.example.activityservicelifecycle;
+
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class MyAsyncTask extends AsyncTask {
+
+  private final static String TAG="LifeCycleAsyncTask";
+  boolean finished = false;
+
+  @Override
+  protected Object doInBackground(Object[] objects) {
+    int i = 0;
+    while (!finished) {
+      SystemClock.sleep(1000);
+      Log.i(TAG, "" + i++);
+    }
+    return null;
+  }
+}
