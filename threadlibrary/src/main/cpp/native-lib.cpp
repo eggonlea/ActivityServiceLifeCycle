cba7b56224576f9bcb73ff215747b631a1ecd5a9
Li Li <eggonlea@msn.com>
2021-09-01 10:53:59 -0700

    test cgroup and flock in native code
    Signed-off-by: Li Li <eggonlea@msn.com>

--- threadlibrary/src/main/cpp/native-lib.cpp
+++ threadlibrary/src/main/cpp/native-lib.cpp
@@ -1,19 +1,103 @@
+#include <fcntl.h>
+#include <fstream>
 #include <jni.h>
 #include <string>
 #include <thread>
 #include <unistd.h>
 #include <android/log.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define TEST_CGROUP false
+#define TEST_FLOCK  false
 
 bool finished = false;
 char tag[64];
 
-void run()
+void test_cgroup()
 {
-    int count = 0;
-    while (!finished) {
-        __android_log_print(ANDROID_LOG_INFO, tag, "%d", count++);
-        sleep(1);
+    int uid=0, pid=0;
+    int self = getpid();
+    std::ifstream cgroup("/proc/self/cgroup");
+    std::string line;
+    while (std::getline(cgroup, line)) {
+        __android_log_print(ANDROID_LOG_INFO, tag, "%s", line.c_str());
+        if (line.find("uid") != std::string::npos) {
+            sscanf(line.c_str(), "0::/uid_%d/pid_%d", &uid, &pid);
+            __android_log_print(ANDROID_LOG_INFO, tag, "pid=%d, uid/pid=%d/%d", self, uid, pid);
+        }
     }
+
+    if (uid > 0 && pid > 0) {
+        int ret;
+        char uiddir[1024];
+        char piddir[1024];
+        snprintf(uiddir, 1024, "/sys/fs/cgroup/uid_%d", uid);
+        snprintf(piddir, 1024, "/sys/fs/cgroup/uid_%d/pid_%d", uid, pid);
+        for (int i = 0; i < 3; i++) {
+            ret = mkdir(uiddir, 0750);
+            __android_log_print(ANDROID_LOG_INFO,
+                                tag,
+                                "mkdir#%d(%s, 0750) returns %d (errno=%d, EEXIST=%d)",
+                                i,
+                                uiddir,
+                                ret,
+                                errno,
+                                EEXIST);
+            ret = mkdir(piddir, 0750);
+            __android_log_print(ANDROID_LOG_INFO,
+                                tag,
+                                "mkdir#%d(%s, 0750) returns %d (errno=%d, EEXIST=%d)",
+                                i,
+                                piddir,
+                                ret,
+                                errno,
+                                EEXIST);
+        }
+    }
+}
+
+void test_flock()
+{
+  // To test the file locks. Do the following before running the app
+  // adb shell touch /data/file_lock
+  // adb shell chmod 777 /data/file_lock
+  // adb shell setenforce 0
+  // And then the blocked locks should be visible in /proc/locks
+  int fd = open("/data/file_lock", O_CREAT | O_RDWR, S_IRWXU);
+  __android_log_print(ANDROID_LOG_INFO, tag, "open %d", fd);
+  fork();
+  fork();
+  struct flock lock;
+  lock.l_type = F_WRLCK;
+  lock.l_start = 0;
+  lock.l_whence = SEEK_SET;
+  lock.l_len = 0;
+  __android_log_print(ANDROID_LOG_INFO, tag, "locking %d", fd);
+  int ret = fcntl(fd, F_SETLKW, &lock);
+  __android_log_print(ANDROID_LOG_INFO, tag, "locked %d return %d", fd, ret);
+  //close(fd);
+}
+
+void run() {
+  if (TEST_CGROUP) {
+    test_cgroup();
+    int pid = fork();
+    __android_log_print(ANDROID_LOG_INFO, tag, "fork %d", pid);
+
+    if (pid == 0) {
+      test_cgroup();
+      return;
+    }
+  }
+
+  if (TEST_FLOCK) test_flock();
+
+  int count = 0;
+  while (!finished) {
+    __android_log_print(ANDROID_LOG_INFO, tag, "%d", count++);
+    sleep(1);
+  }
 }
 
 extern "C" JNIEXPORT void JNICALL
